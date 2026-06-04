bd9aee7052d04930eb64e22af7d2e27c7435456a
M8gic187 <maximilian.buerger12@gmx.de>
2026-06-04 07:10:32 +0000

    feat: export NMF from decomposition module
    Register NMF in the decomposition package __init__ so it is importable
    via `from ml_scratch.decomposition import NMF`.

--- ml_scratch/decomposition/__init__.py
+++ ml_scratch/decomposition/__init__.py
@@ -2,7 +2,8 @@
 
 from .ica import FastICA
 from .lda import LinearDiscriminantAnalysis
+from .nmf import NMF
 from .pca import PCA
 from .tsne import TSNE
 
-__all__ = ["FastICA", "LinearDiscriminantAnalysis", "PCA", "TSNE"]
+__all__ = ["FastICA", "LinearDiscriminantAnalysis", "NMF", "PCA", "TSNE"]
