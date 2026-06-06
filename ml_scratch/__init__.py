855fe8ef2960a58a4beeea44b299ff19ae9bf2a9
M8gic187 <maximilian.buerger12@gmx.de>
2026-06-06 07:11:53 +0000

    feat: export GaussianProcessRegressor from top-level ml_scratch package
    Register GaussianProcessRegressor in ml_scratch/__init__.py so it is
    importable directly via 'from ml_scratch import GaussianProcessRegressor'.

--- ml_scratch/__init__.py
+++ ml_scratch/__init__.py
@@ -1,3 +1,7 @@
 """ml-from-scratch: Machine learning algorithms implemented with NumPy only."""
 
+from .gaussian_process import GaussianProcessRegressor
+
 __version__ = "0.1.0"
+
+__all__ = ["GaussianProcessRegressor"]
